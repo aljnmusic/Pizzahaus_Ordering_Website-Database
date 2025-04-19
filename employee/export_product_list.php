65ecb9f67b721d1f11f3400fb54e169ede70bbfe
aljnmusic <galosjohnaljenne@gmail.com>
2025-04-19 22:23:08 +0800

    419251023

--- employee/export_product_list.php
+++ employee/export_product_list.php
@@ -17,5 +17,13 @@ if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
 
+$sql = "SELECT product_id, name, category, price FROM product";
+$result = $conn->query($sql);
+
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+
+
+
 
 ?>
